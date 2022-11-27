3bddd26d88028ac7f05b809f850213a176ad55f7
solidspoon <cedarrival@outlook.com>
2022-11-27 22:49:18 +0800

    代码完善

--- src/main/controllers/AppleTrans.ts
+++ src/main/controllers/AppleTrans.ts
@@ -2,10 +2,13 @@
  * 调用 Bob 翻译
  * @param word
  */
+import { app } from "electron";
+
 export default function(word: string) {
     if (process.platform !== "darwin") {
         return;
     }
+    console.log(app.getPath('userData'));
     word = word.replace("\"", " ");
     let script: string = "tell application \"Bob\"\n" +
         "launch\n" +
