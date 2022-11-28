057749f803e0798ea9d5d028fc14d4f0605d6f5d
solidspoon <cedarrival@outlook.com>
2022-11-28 21:41:24 +0800

    fix exlink

--- src/renderer/components/BorderProgressBar.tsx
+++ src/renderer/components/BorderProgressBar.tsx
@@ -3,64 +3,61 @@ import { Component } from 'react';
 import s from './css/BorderProgressBar.module.css';
 
 interface BorderProgressBarParam {
-  getCurrentTime: () => number;
-  getTotalTime: () => number;
+    getCurrentTime: () => number;
+    getTotalTime: () => number;
 }
 
 interface BorderProgressBarState {
-  completed: number;
+    completed: number;
 }
 
 export default class BorderProgressBar extends Component<
-  BorderProgressBarParam,
-  BorderProgressBarState
+    BorderProgressBarParam,
+    BorderProgressBarState
 > {
-  private interval: NodeJS.Timer | undefined;
+    private interval: NodeJS.Timer | undefined;
 
-  constructor(
-    props: BorderProgressBarParam | Readonly<BorderProgressBarParam>
-  ) {
-    super(props);
-    this.state = {
-      completed: 100,
-    };
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(this.intervalTask, 100);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  private intervalTask = (): void => {
-    const { getCurrentTime, getTotalTime } = this.props;
-    const currentTime = getCurrentTime();
-    const totalTime = getTotalTime();
-    if (currentTime === undefined || totalTime === undefined) {
-      return;
+    constructor(
+        props: BorderProgressBarParam | Readonly<BorderProgressBarParam>
+    ) {
+        super(props);
+        this.state = {
+            completed: 100,
+        };
     }
-    this.setState({
-      completed: (currentTime / totalTime) * 100,
-    });
-  };
 
-  render() {
-    const { completed } = this.state;
-    return (
-      <div className={s.processBar}>
-        <ProgressBar
-          completed={completed}
-          transitionDuration="0.2s"
-          isLabelVisible={false}
-          height="8px"
-          width="100%"
-        />
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.interval = setInterval(this.intervalTask, 100);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    private intervalTask = (): void => {
+        const { getCurrentTime, getTotalTime } = this.props;
+        const currentTime = getCurrentTime();
+        const totalTime = getTotalTime();
+        if (currentTime === undefined || totalTime === undefined) {
+            return;
+        }
+        this.setState({
+            completed: (currentTime / totalTime) * 100,
+        });
+    };
+
+    render() {
+        const { completed } = this.state;
+        return (
+            <div className={s.processBar}>
+                <ProgressBar
+                    completed={completed}
+                    transitionDuration="0.2s"
+                    isLabelVisible={false}
+                    height="8px"
+                    width="100%"
+                />
+            </div>
+        );
+    }
 }
-/*
-
- */
--- src/renderer/components/GlobalShortCut.tsx
+++ src/renderer/components/GlobalShortCut.tsx
@@ -2,89 +2,89 @@ import { Component, ReactNode } from 'react';
 import Keyevent from 'react-keyevent';
 
 export enum JumpPosition {
-  BEFORE = 0,
-  AFTER = 1,
-  CURRENT = 2,
+    BEFORE = 0,
+    AFTER = 1,
+    CURRENT = 2,
 }
 
-export interface ReactParam {
-  children?: ReactNode;
+interface ReactParam {
+    children: undefined | ReactNode;
 }
 
 interface GlobalShortCutParam extends ReactParam {
-  onJumpTo: (position: JumpPosition) => void;
-  onSpace: () => void;
+    onJumpTo: (position: JumpPosition) => void;
+    onSpace: () => void;
 }
 
 export default class GlobalShortCut extends Component<
-  GlobalShortCutParam,
-  any
+    GlobalShortCutParam,
+    never
 > {
-  public onA = () => {
-    const { onJumpTo } = this.props;
-    onJumpTo(JumpPosition.BEFORE);
-  };
+    public onA = () => {
+        const { onJumpTo } = this.props;
+        onJumpTo(JumpPosition.BEFORE);
+    };
 
-  public onD = () => {
-    const { onJumpTo } = this.props;
-    onJumpTo(JumpPosition.AFTER);
-  };
+    public onD = () => {
+        const { onJumpTo } = this.props;
+        onJumpTo(JumpPosition.AFTER);
+    };
 
-  public onS = () => {
-    const { onJumpTo } = this.props;
-    onJumpTo(JumpPosition.CURRENT);
-  };
+    public onS = () => {
+        const { onJumpTo } = this.props;
+        onJumpTo(JumpPosition.CURRENT);
+    };
 
-  public onLeft = () => {
-    const { onJumpTo } = this.props;
-    onJumpTo(JumpPosition.BEFORE);
-  };
+    public onLeft = () => {
+        const { onJumpTo } = this.props;
+        onJumpTo(JumpPosition.BEFORE);
+    };
 
-  public onRight = () => {
-    const { onJumpTo } = this.props;
-    onJumpTo(JumpPosition.AFTER);
-  };
+    public onRight = () => {
+        const { onJumpTo } = this.props;
+        onJumpTo(JumpPosition.AFTER);
+    };
 
-  public onDown = () => {
-    const { onJumpTo } = this.props;
-    onJumpTo(JumpPosition.CURRENT);
-  };
+    public onDown = () => {
+        const { onJumpTo } = this.props;
+        onJumpTo(JumpPosition.CURRENT);
+    };
 
-  public onSpace = () => {
-    const { onSpace } = this.props;
-    onSpace();
-  };
+    public onSpace = () => {
+        const { onSpace } = this.props;
+        onSpace();
+    };
 
-  public onUp = () => {
-    const { onSpace } = this.props;
-    onSpace();
-  };
+    public onUp = () => {
+        const { onSpace } = this.props;
+        onSpace();
+    };
 
-  public onW = () => {
-    const { onSpace } = this.props;
-    onSpace();
-  };
+    public onW = () => {
+        const { onSpace } = this.props;
+        onSpace();
+    };
 
-  render() {
-    const { children } = this.props;
-    return (
-      <Keyevent
-        className="TopSide"
-        events={{
-          onA: this.onA,
-          onD: this.onD,
-          onS: this.onS,
-          onLeft: this.onLeft,
-          onRight: this.onRight,
-          onDown: this.onDown,
-          onSpace: this.onSpace,
-          onUp: this.onUp,
-          onW: this.onW,
-        }}
-        needFocusing={false}
-      >
-        {children}
-      </Keyevent>
-    );
-  }
+    render() {
+        const { children } = this.props;
+        return (
+            <Keyevent
+                className="TopSide"
+                events={{
+                    onA: this.onA,
+                    onD: this.onD,
+                    onS: this.onS,
+                    onLeft: this.onLeft,
+                    onRight: this.onRight,
+                    onDown: this.onDown,
+                    onSpace: this.onSpace,
+                    onUp: this.onUp,
+                    onW: this.onW,
+                }}
+                needFocusing={false}
+            >
+                {children}
+            </Keyevent>
+        );
+    }
 }
