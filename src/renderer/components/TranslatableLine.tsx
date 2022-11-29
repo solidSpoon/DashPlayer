c640a04446fc1b8dbf93641400163d0d41ef4b14
solidSpoon <cedarrival@outlook.com>
2022-11-29 19:42:59 +0800

    修复 eslint

--- src/renderer/components/TranslatableLine.tsx
+++ src/renderer/components/TranslatableLine.tsx
@@ -1,58 +1,65 @@
-import axios from "axios";
-import { ReactElement } from "react";
-import s from "./css/TranslatableLine.module.css";
+import { ReactElement } from 'react';
+import s from './css/TranslatableLine.module.css';
 
 interface TranslatableSubtitleLineParam {
-    text: string,
-    className?: string
+    text: string;
+    className: string | undefined;
 }
 
 const TranslatableLine = (props: TranslatableSubtitleLineParam) => {
-    if (props.text === undefined) {
+    const { text } = props;
+    if (text === undefined) {
         return <></>;
     }
     let keyIndex = 0;
     const trans = (str: string): void => {
-        console.log("click");
-        window.electron.ipcRenderer.sendMessage("trans-word", [str]);
-        window.electron.ipcRenderer.once("trans-word", () => {
-            console.log("trans word success");
+        console.log('click');
+        window.electron.ipcRenderer.sendMessage('trans-word', [str]);
+        window.electron.ipcRenderer.once('trans-word', () => {
+            console.log('trans word success');
         });
     };
     const notWord = (str: string): ReactElement => {
-        return <span className={s.notWord} key={keyIndex++}>{str}</span>;
+        keyIndex += 1;
+        return (
+            <span className={s.notWord} key={keyIndex}>
+                {str}
+            </span>
+        );
     };
     const word = (str: string): ReactElement => {
         const t = () => trans(str);
-        return <>
-            <span
-                className={s.word}
-                key={keyIndex++}
-                onClick={t}
-            >
-                {str}
-            </span>
-        </>;
+        keyIndex += 1;
+        return (
+            <>
+                {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+                <span className={s.word} key={keyIndex} onClick={t}>
+                    {str}
+                </span>
+            </>
+        );
     };
     const isWord = (str: string): boolean => {
-        const noWordRegex = /[^A-Za-z0-9\-]/;
+        const noWordRegex = /[^A-Za-z0-9-]/;
         return !noWordRegex.test(str);
     };
-    const words: string[] = props.text.split(/((?<=.)(?=[^A-Za-z0-9\-]))|((?<=[^A-Za-z0-9\-])(?=.))/);
+    // eslint-disable-next-line react/destructuring-assignment
+    const words: string[] = props.text.split(
+        /((?<=.)(?=[^A-Za-z0-9-]))|((?<=[^A-Za-z0-9-])(?=.))/
+    );
 
     function ele(): ReactElement[] {
-        return words.map(w => {
+        return words.map((w) => {
             if (isWord(w)) {
                 return word(w);
-            } else {
-                return notWord(w);
             }
+            return notWord(w);
         });
     }
 
     return (
-        // @ts-ignore
-        <div key={1} className={props.className ? props.className : null}>
+        // eslint-disable-next-line react/destructuring-assignment
+        <div key={1} className={props.className}>
             {ele()}
         </div>
     );
--- src/renderer/components/UplodeButton.tsx
+++ src/renderer/components/UplodeButton.tsx
@@ -1,83 +1,82 @@
-import React, { Component, Fragment } from "react";
-import style from "./css/UplodeButtom.module.css";
-import FileT, { FileType } from "../lib/param/FileT";
-import UploadPhotoParam from "../lib/param/UploadPhotoParam";
+import React, { Component, Fragment } from 'react';
+import style from './css/UplodeButtom.module.css';
+import FileT, { FileType } from '../lib/param/FileT';
+import UploadPhotoParam from '../lib/param/UploadPhotoParam';
 
-export default class UploadPhoto extends Component<UploadPhotoParam, any> {
-  private readonly fileInputEl: React.RefObject<HTMLInputElement>;
+export default class UploadPhoto extends Component<UploadPhotoParam, never> {
+    private readonly fileInputEl: React.RefObject<HTMLInputElement>;
 
-  constructor(props: UploadPhotoParam | Readonly<UploadPhotoParam>) {
-    super(props);
-    this.state = {
-      submitLoading: false
-    };
-    this.fileInputEl = React.createRef();
-
-  }
-
-  // @ts-ignore
-  handlePhoto = async (event) => {
-    const files = [...event.target.files];
-    if (files.length === 0) return;
-    await this.setState({ submitLoading: true });
-
-    files.forEach((file, i) => {
-      const fileT = new FileT();
-      fileT.fileName = file.name;
-      fileT.objectUrl = this.getFileUrl(file);
-      const isSrt = file.name.endsWith("srt");
-      if (isSrt) {
-        fileT.fileType = FileType.SUBTITLE;
-      } else {
-        fileT.fileType = FileType.VIDEO;
-      }
-      this.props.onFileChange(fileT);
-    });
-  };
-
-  // @ts-ignore
-  getFileUrl(file): string {
-    // @ts-ignore
-    let url: string = null;
-    // @ts-ignore
-    if (window.createObjectURL !== undefined) {
-      // @ts-ignore
-      url = window.createObjectURL(file);
-    } else if (window.URL !== undefined) {
-      url = window.URL.createObjectURL(file);
-    } else if (window.webkitURL !== undefined) {
-      url = window.webkitURL.createObjectURL(file);
+    constructor(props: UploadPhotoParam | Readonly<UploadPhotoParam>) {
+        super(props);
+        this.fileInputEl = React.createRef();
     }
-    return url;
-  }
 
-  render() {
-    return (
-      <Fragment>
-        <input
-          type="file"
-          multiple
-          ref={this.fileInputEl}    //挂载ref
-          accept=".mp4,.mkv,.srt"    //限制文件类型
-          hidden    //隐藏input
-          onChange={(event) => this.handlePhoto(event)}
-        />
-        <a className={style.button}
-           onClick={() => {
-             // @ts-ignore
-             this.fileInputEl.current.click();		//当点击a标签的时候触发事件
-           }}
-        >
+    handlePhoto = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const files = [...event.target.files];
+        if (files.length === 0) return;
+        const { onFileChange } = this.props;
+        files.forEach((file) => {
+            const fileT = new FileT();
+            fileT.fileName = file.name;
+            fileT.objectUrl = this.getFileUrl(file);
+            const isSrt = file.name.endsWith('srt');
+            if (isSrt) {
+                fileT.fileType = FileType.SUBTITLE;
+            } else {
+                fileT.fileType = FileType.VIDEO;
+            }
+            onFileChange(fileT);
+        });
+    };
 
-          <svg className="icon" viewBox="0 0 1024 1024" version="1.1"
-               xmlns="http://www.w3.org/2000/svg" p-id="5901" width="200" height="200">
-            <path
-              d="M426.666667 426.666667H85.546667A85.418667 85.418667 0 0 0 0 512c0 47.445333 38.314667 85.333333 85.546667 85.333333H426.666667v341.12c0 47.274667 38.186667 85.546667 85.333333 85.546667 47.445333 0 85.333333-38.314667 85.333333-85.546667V597.333333h341.12A85.418667 85.418667 0 0 0 1024 512c0-47.445333-38.314667-85.333333-85.546667-85.333333H597.333333V85.546667A85.418667 85.418667 0 0 0 512 0c-47.445333 0-85.333333 38.314667-85.333333 85.546667V426.666667z"
-              p-id="5902"></path>
-          </svg>
+    getFileUrl = (file: Blob | MediaSource): string => {
+        let url: string;
 
-        </a>
-      </Fragment>
-    );
-  }
+        if (window.URL !== undefined) {
+            url = window.URL.createObjectURL(file);
+        } else if (window.webkitURL !== undefined) {
+            url = window.webkitURL.createObjectURL(file);
+        } else {
+            throw new Error('can not create object url');
+        }
+        return url;
+    };
+
+    render() {
+        return (
+            <>
+                <input
+                    type="file"
+                    multiple
+                    ref={this.fileInputEl} // 挂载ref
+                    accept=".mp4,.mkv,.srt" // 限制文件类型
+                    hidden // 隐藏input
+                    onChange={(event) => this.handlePhoto(event)}
+                />
+                <a
+                    className={style.button}
+                    onClick={() => {
+                        this.fileInputEl.current?.click(); // 当点击a标签的时候触发事件
+                    }}
+                >
+                    <svg
+                        className="icon"
+                        viewBox="0 0 1024 1024"
+                        version="1.1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        p-id="5901"
+                        width="200"
+                        height="200"
+                    >
+                        <path
+                            d="M426.666667 426.666667H85.546667A85.418667 85.418667 0 0 0 0 512c0 47.445333 38.314667 85.333333 85.546667 85.333333H426.666667v341.12c0 47.274667 38.186667 85.546667 85.333333 85.546667 47.445333 0 85.333333-38.314667 85.333333-85.546667V597.333333h341.12A85.418667 85.418667 0 0 0 1024 512c0-47.445333-38.314667-85.333333-85.546667-85.333333H597.333333V85.546667A85.418667 85.418667 0 0 0 512 0c-47.445333 0-85.333333 38.314667-85.333333 85.546667V426.666667z"
+                            p-id="5902"
+                        />
+                    </svg>
+                </a>
+            </>
+        );
+    }
 }
