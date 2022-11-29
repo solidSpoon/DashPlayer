fdcd5c6d51886996bbe959393f19e2f54d279a11
solidSpoon <cedarrival@outlook.com>
2022-11-29 11:38:42 +0800

    修复 eslint

--- src/renderer/lib/searchSubtitle.tsx
+++ src/renderer/lib/searchSubtitle.tsx
@@ -1,8 +1,30 @@
-import SentenceT from "./param/SentenceT";
+import SentenceT from './param/SentenceT';
 
-const searchSubtitle = (subtitles: SentenceT[] , currentTime: number, currentSubtitle: SentenceT): SentenceT => {
-    if (currentSubtitle === undefined || currentSubtitle.fileUrl !== subtitles[0].fileUrl) {
-        return subtitles.find(v => current(v, currentTime)) as SentenceT;
+const current = (s: SentenceT, currentTime: number): boolean => {
+    let { timeEnd } = s;
+    const { timeStart } = s;
+    if (s.nextItem !== undefined) {
+        timeEnd = s.nextItem.timeStart;
+    }
+    if (timeEnd === undefined || timeStart === undefined) {
+        return false;
+    }
+    return currentTime >= timeStart - 0.2 && currentTime <= timeEnd;
+};
+
+const searchSubtitle = (
+    subtitles: SentenceT[] | undefined,
+    currentTime: number,
+    currentSubtitle: SentenceT | undefined
+): SentenceT | undefined => {
+    if (subtitles === undefined) {
+        return undefined;
+    }
+    if (
+        currentSubtitle === undefined ||
+        currentSubtitle.fileUrl !== subtitles[0].fileUrl
+    ) {
+        return subtitles.find((v) => current(v, currentTime)) as SentenceT;
     }
 
     if (current(currentSubtitle, currentTime)) {
@@ -19,14 +41,6 @@ const searchSubtitle = (subtitles: SentenceT[] , currentTime: number, currentSub
             return currentSubtitle.nextItem;
         }
     }
-    return subtitles.find(v => current(v, currentTime)) as SentenceT;
-}
-const current = (s: SentenceT, currentTime: number): boolean => {
-    let timeEnd: number | undefined = s.timeEnd;
-    if (s.nextItem !== undefined) {
-        timeEnd = s.nextItem.timeStart;
-    }
-    // @ts-ignore
-  return currentTime >= s.timeStart - 0.2 && currentTime <= timeEnd;
-}
+    return subtitles.find((v) => current(v, currentTime)) as SentenceT;
+};
 export default searchSubtitle;
