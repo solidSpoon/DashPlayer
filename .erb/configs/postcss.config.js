5482452cb8608ff785788d78b934e0476b99aae3
solidspoon <cedarrival@outlook.com>
2022-12-01 22:06:18 +0800

    eslint

--- .erb/configs/postcss.config.js
+++ .erb/configs/postcss.config.js
@@ -1,5 +1,5 @@
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
     plugins: [tailwindcss, autoprefixer],
