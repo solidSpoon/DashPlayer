41ef7ca3d61b1a30162637794802862d404088a1
solidspoon <cedarrival@outlook.com>
2022-12-01 23:53:36 +0800

    upload button to tailwindcss

--- src/renderer/components/UplodeButton.tsx
+++ src/renderer/components/UplodeButton.tsx
@@ -59,19 +59,17 @@ export default class UploadPhoto extends Component<UploadPhotoParam, never> {
                 />
                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid,jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
                 <a
-                    className={style.button}
+                    className="flex place-content-center bg-lime-800 hover:bg-lime-700 rounded-full w-12 h-12 p-0.5 drop-shadow-md fixed bottom-12 right-12 "
                     onClick={() => {
                         this.fileInputEl.current?.click(); // 当点击a标签的时候触发事件
                     }}
                 >
                     <svg
-                        className="icon"
+                        className="icon m-auto fill-white w-1/2 h-1/2"
                         viewBox="0 0 1024 1024"
                         version="1.1"
                         xmlns="http://www.w3.org/2000/svg"
                         p-id="5901"
-                        width="200"
-                        height="200"
                     >
                         <path
                             d="M426.666667 426.666667H85.546667A85.418667 85.418667 0 0 0 0 512c0 47.445333 38.314667 85.333333 85.546667 85.333333H426.666667v341.12c0 47.274667 38.186667 85.546667 85.333333 85.546667 47.445333 0 85.333333-38.314667 85.333333-85.546667V597.333333h341.12A85.418667 85.418667 0 0 0 1024 512c0-47.445333-38.314667-85.333333-85.546667-85.333333H597.333333V85.546667A85.418667 85.418667 0 0 0 512 0c-47.445333 0-85.333333 38.314667-85.333333 85.546667V426.666667z"
