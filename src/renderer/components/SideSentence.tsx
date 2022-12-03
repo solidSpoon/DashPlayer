71ded79ef981395452c1096610c0b1a2a181b251
solidspoon <cedarrival@outlook.com>
2022-12-04 00:14:23 +0800

    播放按钮样式调整

--- src/renderer/components/SideSentence.tsx
+++ src/renderer/components/SideSentence.tsx
@@ -43,6 +43,7 @@ export default class SideSentence extends Component<SideSentenceParam, any> {
                 py-2
                 border-0
                 flex
+                content-start
                 rounded-lg
                 bg-neutral-700
                 hover:drop-shadow-xl
@@ -59,19 +60,15 @@ export default class SideSentence extends Component<SideSentenceParam, any> {
                 }}
             >
                 <div
-                    className="flex place-content-center w-10 invisible"
+                    className="flex flex-row justify-center justify-items-start w-10 invisible"
                     ref={this.iconRef}
                 >
                     <svg
-                        className="icon w-3/4 h-3/4"
+                        className="icon w-3/4 h-min"
                         viewBox="0 0 1024 1024"
                         version="1.1"
                         xmlns="http://www.w3.org/2000/svg"
                         p-id="3451"
-                        width="30"
-                        height=""
-                        // width="20"
-                        // height="20"
                     >
                         <path
                             d="M684.07 452.22h-0.01l-209.4-120.91c-23.38-13.48-51.27-13.49-74.67 0.01-23.36 13.5-37.32 37.67-37.32 64.66v241.8c0 27 13.96 51.17 37.34 64.67 11.68 6.75 24.5 10.11 37.32 10.11s25.65-3.38 37.33-10.12l209.39-120.89c23.39-13.5 37.34-37.67 37.34-64.66 0.01-26.98-13.94-51.16-37.32-64.67zM448 619.3V414.46l177.4 102.42L448 619.3z"
