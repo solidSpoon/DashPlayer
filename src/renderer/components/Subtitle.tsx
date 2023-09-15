343b74046b10acb860145eabc2a5f889c4e65cf2
solidSpoon <cedarrival@outlook.com>
2023-09-15 22:33:29 +0800

    完成

--- src/renderer/components/Subtitle.tsx
+++ src/renderer/components/Subtitle.tsx
@@ -1,94 +1,94 @@
-import React, { useEffect, useRef } from 'react';
+import React, { Component, useEffect, useRef } from 'react';
 import { Virtuoso, VirtuosoHandle } from 'react-virtuoso';
 import isVisible from '../lib/isVisible';
 import SentenceT from '../lib/param/SentenceT';
 import SideSentenceNew from './SideSentenceNew';
-import { Action } from '../lib/CallAction';
+import { Action, jump, jumpTime } from '../lib/CallAction';
 
 interface SubtitleSubParam {
     subtitles: SentenceT[];
     currentSentence: SentenceT | undefined;
     onAction: (action: Action) => void;
 }
-const isSentenceVisible = (
-    s: SentenceT | undefined,
-    vr: number[],
-    crDiv: HTMLDivElement | null
-): boolean => {
-    if (s === undefined || crDiv === null) {
-        return false;
+
+export default class SubtitleSub extends Component<SubtitleSubParam> {
+    private readonly SCROOL_BOUNDARY = 50;
+
+    private currentRef: React.RefObject<HTMLDivElement> = React.createRef();
+
+    private listRef: React.RefObject<VirtuosoHandle> = React.createRef();
+
+    private visibleRange: [number, number] = [0, 0];
+
+    componentDidUpdate = (
+        _prevProps: Readonly<SubtitleSubParam>,
+        snapshot?: any
+    ) => {
+        // 似乎 render() 最外层元素渲染后就会触发 componentDidUpdate，此时还拿不到新的元素
+        // 所以需要延迟一下
+        requestAnimationFrame(() => {
+            const { currentSentence } = this.props;
+            const beforeVisable = snapshot as boolean;
+            if (
+                this.currentRef.current === null &&
+                currentSentence === undefined
+            ) {
+                return;
+            }
+            const currentVisable = this.isIsVisible();
+            const index = currentSentence?.index;
+            if (index && !currentVisable) {
+                if (beforeVisable) {
+                    this.listRef.current?.scrollToIndex({
+                        index: index - 1 >= 0 ? index - 1 : 0,
+                        align: 'start',
+                        behavior: 'smooth',
+                    });
+                } else {
+                    this.listRef.current?.scrollIntoView({
+                        index,
+                        align: 'start',
+                        behavior: 'auto',
+                    });
+                    this.listRef.current?.scrollIntoView({
+                        index: index - 1 >= 0 ? index - 1 : 0,
+                        align: 'start',
+                        behavior: 'auto',
+                    });
+                }
+            }
+        });
+    };
+
+    getSnapshotBeforeUpdate(): boolean {
+        return this.isIsVisible();
     }
-    const index = s?.index;
-    if (index) {
-        if (index < vr[0] || index > vr[1]) {
-            return false;
-        }
-    }
-    return isVisible(crDiv, 50);
-};
-export default function Subtitle({
-    subtitles,
-    currentSentence,
-    onAction,
-}: SubtitleSubParam): React.ReactElement {
-    const beforeVisible = useRef(true);
-    const beforeSentence = useRef<SentenceT | undefined>(undefined);
-    const currentRef: React.RefObject<HTMLDivElement> = useRef(null);
 
-    const listRef: React.RefObject<VirtuosoHandle> = useRef(null);
-
-    const visibleRange = useRef([0, 0]);
-
-    console.log('vis update', currentSentence?.text ?? '', currentRef.current);
-    beforeVisible.current = isSentenceVisible(
-        beforeSentence.current,
-        visibleRange.current,
-        currentRef.current
-    );
-    beforeSentence.current = currentSentence;
-    useEffect(() => {
-        if (currentRef.current === null && currentSentence === undefined) {
-            return;
-        }
-        const currentVisible = isSentenceVisible(
-            currentSentence,
-            visibleRange.current,
-            currentRef.current
-        );
-        console.log(
-            `visible, cur ${currentVisible}, bef ${beforeVisible.current}`
-        );
+    private isIsVisible = (): boolean => {
+        const { currentSentence } = this.props;
         const index = currentSentence?.index;
-        if (index && !currentVisible) {
-            if (beforeVisible.current) {
-                listRef.current?.scrollToIndex({
-                    index: index - 1 >= 0 ? index - 1 : 0,
-                    align: 'start',
-                    behavior: 'smooth',
-                });
-            } else {
-                listRef.current?.scrollIntoView({
-                    index,
-                    align: 'start',
-                    behavior: 'auto',
-                });
-                listRef.current?.scrollIntoView({
-                    index: index - 1 >= 0 ? index - 1 : 0,
-                    align: 'start',
-                    behavior: 'auto',
-                });
+        if (index) {
+            if (index < this.visibleRange[0] || index > this.visibleRange[1]) {
+                return false;
             }
         }
-    }, [currentSentence, subtitles, visibleRange]);
+        const currentDiv = this.currentRef.current;
+        if (currentDiv === null) {
+            return false;
+        }
+        return isVisible(currentDiv, this.SCROOL_BOUNDARY);
+    };
 
-    const render = () => {
+    render() {
+        const { subtitles } = this.props;
+        const { currentSentence, onAction } = this.props;
         return (
             <Virtuoso
-                ref={listRef}
+                ref={this.listRef}
                 className="h-full w-full"
                 data={subtitles}
                 rangeChanged={({ startIndex, endIndex }) => {
-                    visibleRange.current = [startIndex, endIndex];
+                    this.visibleRange = [startIndex, endIndex];
                 }}
                 itemContent={(index, item) => {
                     const isCurrent = item.equals(currentSentence);
@@ -99,20 +99,17 @@ export default function Subtitle({
                         >
                             <SideSentenceNew
                                 sentence={item}
-                                onClick={(sentence) =>
-                                    onAction({
-                                        action: 'jump',
-                                        target: sentence,
-                                        time: undefined,
-                                    })
-                                }
+                                onClick={(sentence) => onAction(jump(sentence))}
                                 isCurrent={isCurrent}
                             />
                         </div>
                     );
                     if (isCurrent) {
                         result = (
-                            <div key={`${item.getKey()}div`} ref={currentRef}>
+                            <div
+                                key={`${item.getKey()}div`}
+                                ref={this.currentRef}
+                            >
                                 {result}
                             </div>
                         );
@@ -121,7 +118,5 @@ export default function Subtitle({
                 }}
             />
         );
-    };
-
-    return render();
+    }
 }
