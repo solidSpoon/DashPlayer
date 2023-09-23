cb8f429839088c7fc1e22a4240ef03787dd99d64
solidSpoon <cedarrival@outlook.com>
2023-09-23 21:34:24 +0800

    单词 pop 报错

--- src/renderer/components/WordPop.tsx
+++ src/renderer/components/WordPop.tsx
@@ -64,54 +64,55 @@ const WordPop = React.forwardRef(
             if (!translation) {
                 return <div className="text-2xl">loading</div>;
             }
+            const { basic } = translation;
             return (
-                <div className="relative top-0 left-0 max-w-sm max-h-96 overflow-y-auto flex flex-col items-start  bg-gray-200 text-gray-900 shadow-inner shadow-gray-100 drop-shadow-2xl rounded-2xl pt-4 px-4">
-                    <div className="text-2xl mb-2 flex justify-start items-center gap-4">
-                        {translation.query}
-                        <div>{translation.translation}</div>
-                    </div>
-                    {translation?.basic && (
+                <div
+                    className={`relative top-0 left-0 max-w-sm max-h-96 overflow-y-auto flex flex-col items-start  bg-gray-200 text-gray-900 shadow-inner shadow-gray-100 drop-shadow-2xl rounded-2xl px-4 ${
+                        basic && 'pt-4'
+                    }`}
+                >
+                    {basic && (
                         <>
+                            <div className="text-2xl mb-2 flex justify-start items-center gap-4">
+                                {translation.query}
+                                <div>{translation.translation}</div>
+                            </div>
                             <div className="pl-2 text-base flex justify-start items-center gap-2">
-                                {`美 [${
-                                    translation?.basic['us-phonetic'] || ''
-                                }]`}
+                                {`美 [${basic['us-phonetic'] || ''}]`}
                                 <AiOutlineSound
                                     onClick={() => play('us')}
                                     className="cursor-pointer hover:text-gray-400 text-2xl"
                                 />
                             </div>
                             <div className="pl-2 text-base flex justify-start items-start gap-2">
-                                {`英 [${
-                                    translation.basic['uk-phonetic'] ?? ''
-                                }]`}
+                                {`英 [${basic['uk-phonetic'] ?? ''}]`}
                                 <AiOutlineSound
                                     onClick={() => play('uk')}
                                     className="cursor-pointer hover:text-gray-400 text-2xl"
                                 />
                             </div>
+                            <div className="text-base mt-2 flex flex-col gap-2 items-start w-full">
+                                {basic.explains.map((e) => {
+                                    return (
+                                        <div className="p-2 rounded text-left w-full bg-gray-300/25">
+                                            {e}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                            {basic.exam_type && (
+                                <>
+                                    <div className="text-sm text-gray-400 mt-2 mb-1">
+                                        标签
+                                    </div>
+                                    <div className="text-sm">
+                                        {basic.exam_type.join('/')}
+                                    </div>
+                                </>
+                            )}
                         </>
                     )}
 
-                    <div className="text-base mt-2 flex flex-col gap-2 items-start w-full">
-                        {translation.basic?.explains.map((e) => {
-                            return (
-                                <div className="p-2 rounded text-left w-full bg-gray-300/25">
-                                    {e}
-                                </div>
-                            );
-                        })}
-                    </div>
-                    {translation.basic?.exam_type && (
-                        <>
-                            <div className="text-sm text-gray-400 mt-2 mb-1">
-                                标签
-                            </div>
-                            <div className="text-sm">
-                                {translation.basic.exam_type.join('/')}
-                            </div>
-                        </>
-                    )}
                     <div className="sticky bottom-0 text-cyan-900 text-lg text-center bg-gray-200/95 w-full pt-1 mt-1 pb-2">
                         {translation.translation}
                     </div>
