d0f2d13e785ab33822680d21fcb96242c4b12abf
solidSpoon <cedarrival@outlook.com>
2023-09-23 11:52:04 +0800

    mac titlebar color

--- src/renderer/components/TitleBarMac.tsx
+++ src/renderer/components/TitleBarMac.tsx
@@ -40,8 +40,8 @@ const TitleBarMac = ({ title, show }: TitleBarProps) => {
         <div onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
             {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
             <div
-                className={`drag w-full h-10 absolute top-0 z-50 content-center flex flex-col justify-center items-center select-none ${
-                    showTitleBar ? 'bg-amber-300' : ''
+                className={`drag w-full h-10 absolute top-0 z-50 content-center text-black flex flex-col justify-center items-center select-none ${
+                    showTitleBar ? 'bg-stone-200 shadow-inner shadow-stone-100' : ''
                 }`}
                 onDoubleClick={() => {
                     onDoubleClick();
