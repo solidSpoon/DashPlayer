a5d13c715a900a0ba9418d5aced36418c301cb1f
solidSpoon <cedarrival@outlook.com>
2023-10-08 13:36:21 +0800

    删除无用依赖

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 const colors = require('tailwindcss/colors');
-const themer = require('tailwindcss-themer');
 const scrollbar = require('tailwind-scrollbar');
 const { fontSize } = require('tailwindcss/defaultTheme');
 
@@ -9,65 +8,53 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                sky: colors.sky,
-                cyan: colors.cyan,
+                /**
+                 * 背景色
+                 */
+                background: colors.neutral['800'],
+                /**
+                 * 滚动条背景色
+                 */
+                scrollbarTrack: colors.stone['600'],
+                /**
+                 * 滚动条把手色
+                 */
+                scrollbarThumb: colors.stone['400'],
+                /**
+                 * 滚动条把手色 hover
+                 */
+                scrollbarThumbHover: colors.stone['300'],
+                /**
+                 * 进度条完成色
+                 */
+                progressbarComplete: colors.purple['800'],
+                /**
+                 * gutter 背景色
+                 */
+                gutterBackground: colors.stone['100'],
+                /**
+                 * 标题栏色
+                 */
+                titlebar: colors.stone['600'],
+                /**
+                 * 标题栏色 hover
+                 */
+                titlebarHover: colors.stone['500'],
+                /**
+                 * 标题栏文字色
+                 */
+                titlebarText: colors.stone['100'],
+            },
+            fontSize: {
+                mainSubtitleOne: fontSize['3xl'],
+                mainSubtitleTwo: fontSize['3xl'],
+                mainSubtitleThree: fontSize['2xl'],
+                subtitle: fontSize.lg,
             },
         },
     },
     variants: {
         extend: {},
     },
-    plugins: [
-        scrollbar({ nocompatible: true }),
-        themer({
-            defaultTheme: {
-                extend: {
-                    colors: {
-                        /**
-                         * 背景色
-                         */
-                        background: colors.neutral['800'],
-                        /**
-                         * 滚动条背景色
-                         */
-                        scrollbarTrack: colors.stone['600'],
-                        /**
-                         * 滚动条把手色
-                         */
-                        scrollbarThumb: colors.stone['400'],
-                        /**
-                         * 滚动条把手色 hover
-                         */
-                        scrollbarThumbHover: colors.stone['300'],
-                        /**
-                         * 进度条完成色
-                         */
-                        progressbarComplete: colors.purple['800'],
-                        /**
-                         * gutter 背景色
-                         */
-                        gutterBackground: colors.stone['100'],
-                        /**
-                         * 标题栏色
-                         */
-                        titlebar: colors.stone['600'],
-                        /**
-                         * 标题栏色 hover
-                         */
-                        titlebarHover: colors.stone['500'],
-                        /**
-                         * 标题栏文字色
-                         */
-                        titlebarText: colors.stone['100'],
-                    },
-                    fontSize: {
-                        mainSubtitleOne: fontSize['3xl'],
-                        mainSubtitleTwo: fontSize['3xl'],
-                        mainSubtitleThree: fontSize['2xl'],
-                        subtitle: fontSize.lg,
-                    },
-                },
-            },
-        }),
-    ],
+    plugins: [scrollbar({ nocompatible: true })],
 };
