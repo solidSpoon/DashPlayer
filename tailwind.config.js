b5c0e33e17548e44b2571bb7a0f8417fb18dd686
solidSpoon <cedarrival@outlook.com>
2023-10-08 19:50:57 +0800

    Revert "删除无用依赖"
    This reverts commit a5d13c715a900a0ba9418d5aced36418c301cb1f.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const colors = require('tailwindcss/colors');
+const themer = require('tailwindcss-themer');
 const scrollbar = require('tailwind-scrollbar');
 const { fontSize } = require('tailwindcss/defaultTheme');
 
@@ -8,53 +9,65 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                /**
-                 * 背景色
-                 */
-                background: colors.neutral['800'],
-                /**
-                 * 滚动条背景色
-                 */
-                scrollbarTrack: colors.stone['600'],
-                /**
-                 * 滚动条把手色
-                 */
-                scrollbarThumb: colors.stone['400'],
-                /**
-                 * 滚动条把手色 hover
-                 */
-                scrollbarThumbHover: colors.stone['300'],
-                /**
-                 * 进度条完成色
-                 */
-                progressbarComplete: colors.purple['800'],
-                /**
-                 * gutter 背景色
-                 */
-                gutterBackground: colors.stone['100'],
-                /**
-                 * 标题栏色
-                 */
-                titlebar: colors.stone['600'],
-                /**
-                 * 标题栏色 hover
-                 */
-                titlebarHover: colors.stone['500'],
-                /**
-                 * 标题栏文字色
-                 */
-                titlebarText: colors.stone['100'],
-            },
-            fontSize: {
-                mainSubtitleOne: fontSize['3xl'],
-                mainSubtitleTwo: fontSize['3xl'],
-                mainSubtitleThree: fontSize['2xl'],
-                subtitle: fontSize.lg,
+                sky: colors.sky,
+                cyan: colors.cyan,
             },
         },
     },
     variants: {
         extend: {},
     },
-    plugins: [scrollbar({ nocompatible: true })],
+    plugins: [
+        scrollbar({ nocompatible: true }),
+        themer({
+            defaultTheme: {
+                extend: {
+                    colors: {
+                        /**
+                         * 背景色
+                         */
+                        background: colors.neutral['800'],
+                        /**
+                         * 滚动条背景色
+                         */
+                        scrollbarTrack: colors.stone['600'],
+                        /**
+                         * 滚动条把手色
+                         */
+                        scrollbarThumb: colors.stone['400'],
+                        /**
+                         * 滚动条把手色 hover
+                         */
+                        scrollbarThumbHover: colors.stone['300'],
+                        /**
+                         * 进度条完成色
+                         */
+                        progressbarComplete: colors.purple['800'],
+                        /**
+                         * gutter 背景色
+                         */
+                        gutterBackground: colors.stone['100'],
+                        /**
+                         * 标题栏色
+                         */
+                        titlebar: colors.stone['600'],
+                        /**
+                         * 标题栏色 hover
+                         */
+                        titlebarHover: colors.stone['500'],
+                        /**
+                         * 标题栏文字色
+                         */
+                        titlebarText: colors.stone['100'],
+                    },
+                    fontSize: {
+                        mainSubtitleOne: fontSize['3xl'],
+                        mainSubtitleTwo: fontSize['3xl'],
+                        mainSubtitleThree: fontSize['2xl'],
+                        subtitle: fontSize.lg,
+                    },
+                },
+            },
+        }),
+    ],
 };
