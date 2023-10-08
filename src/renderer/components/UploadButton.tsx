53b7d807fda794472103ac6ecdb26302fcaa4a6c
solidSpoon <cedarrival@outlook.com>
2023-10-08 17:13:58 +0800

    优化代码

--- src/renderer/components/UploadButton.tsx
+++ src/renderer/components/UploadButton.tsx
@@ -1,6 +1,6 @@
-import React, { Component, Fragment, useRef, useState } from 'react';
-import FileT, { FileType } from '../lib/param/FileT';
+import React, { useRef } from 'react';
 import UploadPhotoParam from '../lib/param/UploadPhotoParam';
+import FileT, { FileType } from '../lib/param/FileT';
 
 const getFileUrl = (file: Blob | MediaSource): string => {
     let url: string;
@@ -20,11 +20,10 @@ export default function UploadButton({
 }: UploadPhotoParam) {
     const fileInputEl = useRef<HTMLInputElement>(null);
 
-    const handlePhoto = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleFile = async (event: React.ChangeEvent<HTMLInputElement>) => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         const files = [...event.target.files];
-        console.log('files', files);
         if (files.length === 0) return;
         files.forEach((file) => {
             const fileT = new FileT();
@@ -48,26 +47,22 @@ export default function UploadButton({
                 ref={fileInputEl} // 挂载ref
                 accept=".mp4,.mkv,.srt,.webm" // 限制文件类型
                 hidden // 隐藏input
-                onChange={(event) => handlePhoto(event)}
+                onChange={(event) => handleFile(event)}
             />
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid,jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
             <a
-                className="flex place-content-center bg-lime-700 hover:bg-lime-600 rounded-full w-12 h-12 p-0.5 drop-shadow-md fixed bottom-12 right-12 z-50"
+                className="flex items-center justify-center bg-lime-700/80 hover:bg-lime-700/90 rounded-full w-12 h-12 drop-shadow-md fixed bottom-12 right-12 z-50"
                 onClick={() => {
                     fileInputEl.current?.click(); // 当点击a标签的时候触发事件
                 }}
             >
                 <svg
-                    className="icon m-auto fill-white w-1/2 h-1/2"
+                    className="icon fill-gray-100 w-1/2 h-1/2"
                     viewBox="0 0 1024 1024"
                     version="1.1"
                     xmlns="http://www.w3.org/2000/svg"
-                    p-id="5901"
                 >
-                    <path
-                        d="M426.666667 426.666667H85.546667A85.418667 85.418667 0 0 0 0 512c0 47.445333 38.314667 85.333333 85.546667 85.333333H426.666667v341.12c0 47.274667 38.186667 85.546667 85.333333 85.546667 47.445333 0 85.333333-38.314667 85.333333-85.546667V597.333333h341.12A85.418667 85.418667 0 0 0 1024 512c0-47.445333-38.314667-85.333333-85.546667-85.333333H597.333333V85.546667A85.418667 85.418667 0 0 0 512 0c-47.445333 0-85.333333 38.314667-85.333333 85.546667V426.666667z"
-                        p-id="5902"
-                    />
+                    <path d="M426.666667 426.666667H85.546667A85.418667 85.418667 0 0 0 0 512c0 47.445333 38.314667 85.333333 85.546667 85.333333H426.666667v341.12c0 47.274667 38.186667 85.546667 85.333333 85.546667 47.445333 0 85.333333-38.314667 85.333333-85.546667V597.333333h341.12A85.418667 85.418667 0 0 0 1024 512c0-47.445333-38.314667-85.333333-85.546667-85.333333H597.333333V85.546667A85.418667 85.418667 0 0 0 512 0c-47.445333 0-85.333333 38.314667-85.333333 85.546667V426.666667z" />
                 </svg>
             </a>
         </>
