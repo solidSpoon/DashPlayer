0a95dac7058826ef19fa4c5a743e384b8725abe7
solidSpoon <cedarrival@outlook.com>
2023-10-17 10:01:23 +0800

    播放控制条

--- src/renderer/components/PlayerControlPannel.tsx
+++ src/renderer/components/PlayerControlPannel.tsx
@@ -1,4 +1,3 @@
-import ReactSlider from 'react-slider';
 import React, { useEffect, useRef, useState } from 'react';
 import { FaPause, FaPlay } from 'react-icons/fa';
 import { Slider } from './Slider';
@@ -27,11 +26,12 @@ const PlayerControlPannel = ({
     volume,
     onVolumeChange,
 }: PlayerControlPannelProps) => {
-    const [mouseOver, setMouseOver] = useState<boolean>(false);
+    const [mouseOverOut, setMouseOverOut] = useState<boolean>(false);
     const [maxValue, setMaxValue] = useState(0);
     const [currentValue, setCurrentValue] = useState(0);
     const [selecting, setSelecting] = useState(false);
-    const mouseOverTimeout = useRef<NodeJS.Timeout | undefined>(undefined);
+    const mouseOverTimeout = useRef<number[]>([0]);
+
     // const currentValueUpdateTime
     useEffect(() => {
         const update = () => {
@@ -45,81 +45,115 @@ const PlayerControlPannel = ({
             clearInterval(timeout);
         };
     }, [getTotalTime, getCurrentTime, currentValue, selecting]);
-
-    const onMouseOver = () => {
-        if (mouseOverTimeout.current !== undefined) {
-            clearTimeout(mouseOverTimeout.current);
-        }
-        mouseOverTimeout.current = setTimeout(() => {
-            setMouseOver(true);
-        }, 200);
-    };
     const onMouseLeave = () => {
-        if (mouseOverTimeout.current !== undefined) {
-            clearTimeout(mouseOverTimeout.current);
+        while (mouseOverTimeout.current.length > 0) {
+            window.clearTimeout(mouseOverTimeout.current.pop());
         }
-        mouseOverTimeout.current = setTimeout(() => {
-            setMouseOver(false);
-        }, 0);
+        setMouseOverOut(false);
+    };
+
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+        console.log('handleMouseMove', e);
+        while (mouseOverTimeout.current.length > 0) {
+            window.clearTimeout(mouseOverTimeout.current.pop());
+        }
+        if (!mouseOverOut) {
+            const timeout = window.setTimeout(() => setMouseOverOut(true), 100);
+            if (mouseOverTimeout.current.push) {
+                mouseOverTimeout.current.push(timeout);
+            } else {
+                mouseOverTimeout.current = [timeout];
+            }
+        }
+        const timeout = window.setTimeout(() => {
+            setMouseOverOut(false);
+        }, 2000);
+        if (mouseOverTimeout.current.push) {
+            mouseOverTimeout.current.push(timeout);
+        } else {
+            mouseOverTimeout.current = [timeout];
+        }
+    };
+
+    const handleMouseMoveIn = () => {
+        while (mouseOverTimeout.current.length > 0) {
+            window.clearTimeout(mouseOverTimeout.current.pop());
+        }
+        if (!mouseOverOut) {
+            const timeout = window.setTimeout(() => setMouseOverOut(true), 100);
+            if (mouseOverTimeout.current.push) {
+                mouseOverTimeout.current.push(timeout);
+            } else {
+                mouseOverTimeout.current = [timeout];
+            }
+        }
     };
 
     return (
         <div
-            onMouseOver={onMouseOver}
+            onMouseMove={handleMouseMove}
             onMouseLeave={onMouseLeave}
-            className={`w-full flex flex-col-reverse pt-10 h-1/2 ${className}`}
+            className={`w-full flex flex-col-reverse pt-10 h-full ${className}`}
         >
             <div
-                className={`flex flex-col items-center w-full gap-2 h-16 ${
-                    mouseOver ? '' : 'invisible'
-                }`}
+                onMouseMove={(e) => {
+                    e.stopPropagation();
+                    handleMouseMoveIn();
+                }}
+                onMouseLeave={() => {}}
             >
-                <Slider
-                    className="bg-white/50"
-                    max={maxValue}
-                    min={0}
-                    value={[currentValue]}
-                    onValueChange={(value) => {
-                        console.log('onValueChange', value);
-                        setCurrentValue(value[0]);
-                        setSelecting(true);
-                        onPause?.();
-                    }}
-                    onValueCommit={(value) => {
-                        onTimeChange?.(value[0]);
-                        setSelecting(false);
-                    }}
-                />
-                <div className="w-full flex justify-between px-3">
-                    <div className="flex gap-4">
-                        <div
-                            onClick={() => {
-                                if (playing) {
-                                    onPause?.();
-                                } else {
-                                    onPlay?.();
-                                }
-                            }}
-                            className="flex justify-center w-10 h-10 bg-white/20 items-center rounded-lg"
-                        >
-                            {playing ? (
-                                <FaPause className="w-6 h-6 text-white/80" />
-                            ) : (
-                                <FaPlay className="w-6 h-6 text-white/80" />
-                            )}
+                <div
+                    className={`flex flex-col items-center w-full gap-2 h-16 ${
+                        mouseOverOut ? '' : 'invisible'
+                    }`}
+                >
+                    <Slider
+                        className="bg-white/50"
+                        max={maxValue}
+                        min={0}
+                        value={[currentValue]}
+                        onValueChange={(value) => {
+                            console.log('onValueChange', value);
+                            setCurrentValue(value[0]);
+                            setSelecting(true);
+                            onPause?.();
+                        }}
+                        onValueCommit={(value) => {
+                            onTimeChange?.(value[0]);
+                            setSelecting(false);
+                        }}
+                    />
+                    <div className="w-full flex justify-between px-3">
+                        <div className="flex gap-4">
+                            <div
+                                onClick={() => {
+                                    if (playing) {
+                                        onPause?.();
+                                    } else {
+                                        onPlay?.();
+                                    }
+                                }}
+                                className="flex justify-center w-10 h-10 bg-white/20 items-center rounded-lg"
+                            >
+                                {playing ? (
+                                    <FaPause className="w-6 h-6 text-white/80" />
+                                ) : (
+                                    <FaPlay className="w-6 h-6 text-white/80" />
+                                )}
+                            </div>
+                            <div className=" h-full flex items-center">
+                                {`${secondToDate(
+                                    currentValue
+                                )} / ${secondToDate(maxValue)}`}
+                            </div>
                         </div>
-                        <div className=" h-full flex items-center">
-                            {`${secondToDate(currentValue)} / ${secondToDate(
-                                maxValue
-                            )}`}
+                        <div className="h-full flex-1" />
+                        <div className="flex justify-center items-center">
+                            <VolumeSlider
+                                volume={volume ?? 1}
+                                onVolumeChange={onVolumeChange}
+                            />
                         </div>
-                    </div>
-                    <div className="h-full flex-1" />
-                    <div className="flex justify-center items-center">
-                        <VolumeSlider
-                            volume={volume ?? 1}
-                            onVolumeChange={onVolumeChange}
-                        />
                     </div>
                 </div>
             </div>
