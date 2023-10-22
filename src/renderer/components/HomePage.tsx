8c038fbf24423f7d45644ffba0a8472aa6120c42
solidSpoon <cedarrival@outlook.com>
2023-10-22 14:18:22 +0800

    homepage 亮色主题

--- src/renderer/components/HomePage.tsx
+++ src/renderer/components/HomePage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { GoFile, GoHistory } from 'react-icons/go';
+import { twMerge } from 'tailwind-merge';
 import { logo } from '../../pic/img';
 import useSystem from '../hooks/useSystem';
 import TitleBar from './TitleBar/TitleBar';
@@ -8,11 +9,17 @@ import parseFile, { pathToFile } from '../lib/FileParser';
 import { secondToDate } from './PlayTime';
 import useFile from '../hooks/useFile';
 import loading from '../../pic/loading.svg';
+import useSetting from '../hooks/useSetting';
+import { THEME } from '../../types/Types';
+import { cn } from '../../utils/Util';
 
 const api = window.electron;
 
 const HomePage = () => {
     const appVersion = useSystem((s) => s.appVersion);
+    const themeName = useSetting((s) => s.appearance.theme);
+    const dark =
+        THEME[THEME.map((t) => t.name).indexOf(themeName) ?? 0].type === 'dark';
     const [recentPlaylists, setRecentPlaylists] = useState<ProgressParam[]>([]);
     const fileInputEl = useRef<HTMLInputElement>(null);
     const [currentClick, setCurrentClick] = useState<string | undefined>(
@@ -54,14 +61,24 @@ const HomePage = () => {
     const restPlay = recentPlaylists.length > 1 ? recentPlaylists.slice(1) : [];
 
     return (
-        <div className="w-full h-screen flex-1 bg-background flex justify-center items-center select-none overflow-hidden">
+        <div
+            className={cn(
+                'w-full h-screen flex-1 bg-gray-200 flex justify-center items-center select-none overflow-hidden text-black/80',
+                dark && 'bg-neutral-800 text-white/80'
+            )}
+        >
             <TitleBar
                 maximizable={false}
                 className="fixed top-0 left-0 w-full z-50"
                 windowsButtonClassName="hover:bg-titlebarHover"
                 autoHideOnMac={false}
             />
-            <div className="w-1/3 h-full flex flex-col justify-center items-center bg-white/20 rounded-l-lg gap-14 drop-shadow shadow-black">
+            <div
+                className={cn(
+                    'w-1/3 h-full flex flex-col justify-center items-center bg-white/20 rounded-l-lg gap-14 drop-shadow shadow-white',
+                    dark && 'shadow-black'
+                )}
+            >
                 <div className="relative top-0 left-0 w-32 h-32">
                     <img
                         src={logo}
@@ -70,12 +87,28 @@ const HomePage = () => {
                     />
                 </div>
                 <div className="flex flex-col items-center justify-center gap-2">
-                    <h2 className="text-lg text-white/80">DashPlayer</h2>
-                    <text className="text-white/75">{appVersion}</text>
+                    <h2
+                        className={cn(
+                            'text-lg text-black/80',
+                            dark && 'text-white/80'
+                        )}
+                    >
+                        DashPlayer
+                    </h2>
+                    <text
+                        className={cn('text-black/75', dark && 'text-white/75')}
+                    >
+                        {appVersion}
+                    </text>
                 </div>
                 <div className="w-full h-16" />
             </div>
-            <div className="h-full flex-1 w-0 flex flex-col justify-center items-center bg-white/10 rounded-r-lg border-l border-background pl-8 pr-10 gap-6">
+            <div
+                className={cn(
+                    'h-full flex-1 w-0 flex flex-col justify-center items-center bg-stone-300/50 rounded-r-lg border-l border-stone-400 pl-8 pr-10 gap-6',
+                    dark && 'border-neutral-800 bg-white/10'
+                )}
+            >
                 <div className="w-full h-10" />
                 <input
                     type="file"
@@ -87,21 +120,37 @@ const HomePage = () => {
                 />
                 <div
                     onClick={() => fileInputEl.current?.click()}
-                    className="w-full hover:bg-white/10 px-4 h-12 rounded-lg flex items-center justify-start"
+                    className={cn(
+                        'w-full hover:bg-black/10 px-4 h-12 rounded-lg flex items-center justify-start',
+                        dark && 'hover:bg-white/10'
+                    )}
                 >
                     Open Files...
                 </div>
                 {lastPlay && (
                     <div
                         onClick={() => handleClick(lastPlay)}
-                        className="w-full bg-white/10 hover:bg-white/20 px-4 h-12 rounded-lg flex items-center justify-start gap-2 text-sm"
+                        className={cn(
+                            'w-full bg-black/10 hover:bg-black/20 px-4 h-12 rounded-lg flex items-center justify-start gap-2 text-sm',
+                            dark && 'bg-white/10 hover:bg-white/20'
+                        )}
                     >
-                        <GoHistory className="w-4 h-4 fill-neutral-400" />
+                        <GoHistory
+                            className={cn(
+                                'w-4 h-4 fill-neutral-600',
+                                dark && 'fill-neutral-400'
+                            )}
+                        />
                         <span>Resume</span>
                         <span className="flex-1 truncate">
                             {lastPlay.fileName}
                         </span>
-                        <span className="text-neutral-400">
+                        <span
+                            className={cn(
+                                'text-neutral-600',
+                                dark && 'text-neutral-400'
+                            )}
+                        >
                             {secondToDate(lastPlay.progress)}
                         </span>
                     </div>
@@ -111,9 +160,17 @@ const HomePage = () => {
                         <div
                             key={playlist.fileName}
                             onClick={() => handleClick(playlist)}
-                            className="w-full h-10 flex-shrink-0 flex justify-center items-center hover:bg-white/5 rounded-lg gap-3 px-6"
+                            className={cn(
+                                'w-full h-10 flex-shrink-0 flex justify-center items-center hover:bg-black/5 rounded-lg gap-3 px-6',
+                                dark && 'hover:bg-white/5'
+                            )}
                         >
-                            <GoFile className="w-4 h-4 fill-yellow-400/70" />
+                            <GoFile
+                                className={cn(
+                                    'w-4 h-4 fill-yellow-700/90',
+                                    dark && 'fill-yellow-400/70'
+                                )}
+                            />
                             <div className="w-full truncate">
                                 {playlist.fileName}
                             </div>
