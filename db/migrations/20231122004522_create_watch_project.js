b404a521378a50472325e5be1dbe732405996dbc
solidSpoon <cedarrival@outlook.com>
2023-12-02 12:07:07 +0800

    backup

--- db/migrations/20231122004522_create_watch_project.js
+++ db/migrations/20231122004522_create_watch_project.js
@@ -1,4 +1,7 @@
-import { createAtTimestampTrigger, toTrimLowerCaseTrigger, updateAtTimestampTrigger } from './util/util.js';
+import {
+    createAtTimestampTrigger,
+    updateAtTimestampTrigger,
+} from './util/util.js';
 
 const WATCH_PROJECT_TABLE_NAME = 'dp_watch_project';
 export async function up(knex) {
@@ -9,8 +12,7 @@ export async function up(knex) {
             table.integer('type').notNullable().defaultTo(0);
             table.string('project_key').notNullable();
             table.string('project_path', 1024).notNullable();
-            table.integer('current_video_id').notNullable().defaultTo(0);
-            //watch time
+            // watch time
             table.integer('last_watch_time').notNullable().defaultTo(0);
             // create_at and update_at
             table.timestamps(true, false);
@@ -24,8 +26,6 @@ export async function up(knex) {
         });
 }
 
-
 export async function down(knex) {
     return knex.schema.dropTable(WATCH_PROJECT_TABLE_NAME);
 }
-
--- src/renderer/components/TitleBar/TitleBarMac.tsx
+++ src/renderer/components/TitleBar/TitleBarMac.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import useSystem from '../../hooks/useSystem';
+import { cn } from '../../../utils/Util';
 
 export interface TitleBarProps {
     hasSubtitle?: boolean;
@@ -67,30 +69,50 @@ const TitleBarMac = ({
 
     return (
         // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
-        <div onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+        <div className={cn('fixed h-10 drag w-full top-0 left-0 z-50')}>
             {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
+            <AnimatePresence>
+                {showTitleBar && (
+                    <>
+                        <motion.div
+                            initial={{ y: -100 }}
+                            animate={{ y: 0 }}
+                            exit={{ y: -100 }}
+                            transition={{
+                                duration: 0.2,
+                                ease: 'easeInOut',
+                            }}
+                            className={cn(
+                                `drag w-full h-full content-center text-titlebarText grid place-content-center items-center select-none`,
+                                showTitleBar && className,
+                                hasSubtitle && '-translate-x-2'
+                            )}
+                            onDoubleClick={() => {
+                                onDoubleClick();
+                            }}
+                        >
+                            {title}
+                        </motion.div>
+                        {hasSubtitle && (
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 1 1"
+                                className={cn(
+                                    `absolute top-0 right-0 w-1 h-1 fill-scrollbarTrack -translate-x-2 rotate-180`,
+                                    'translate-y-10'
+                                )}
+                            >
+                                <path d="M 0 0 L 0 1 L 1 1 C 0 1 0 0 0 0 Z" />
+                            </svg>
+                        )}
+                    </>
+                )}
+            </AnimatePresence>
             <div
-                className={`drag w-full h-10 absolute top-0 z-50 content-center text-titlebarText flex flex-col justify-center items-center select-none ${
-                    showTitleBar ? className : ''
-                } ${hasSubtitle ? '-translate-x-2' : ''}`}
-                onDoubleClick={() => {
-                    onDoubleClick();
-                }}
-            >
-                {showTitleBar ? title : ''}
-            </div>
-
-            {hasSubtitle && (
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 1 1"
-                    className={`absolute top-0 right-0 w-1 h-1 fill-scrollbarTrack -translate-x-2 rotate-180
-                        ${showTitleBar ? 'translate-y-10' : ''}
-                `}
-                >
-                    <path d="M 0 0 L 0 1 L 1 1 C 0 1 0 0 0 0 Z" />
-                </svg>
-            )}
+                onMouseOver={handleMouseOver}
+                onMouseLeave={handleMouseLeave}
+                className={cn('w-full h-10 absolute top-0')}
+            />
         </div>
     );
 };
