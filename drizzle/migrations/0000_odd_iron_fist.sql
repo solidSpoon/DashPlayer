4a3c5c4b0b2b9e9f4e0b544fa92e3c2ce105d6c3
solidSpoon <cedarrival@outlook.com>
2023-12-03 19:59:16 +0800

    backup

---
+++ drizzle/migrations/0000_odd_iron_fist.sql
@@ -0,0 +1,72 @@
+CREATE TABLE `dp_kvs` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`key` text NOT NULL,
+	`value` text,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `dp_sentence_translates` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`sentence` text NOT NULL,
+	`translate` text,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `dp_stems` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`stem` text NOT NULL,
+	`familiar` integer DEFAULT false NOT NULL,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `dp_watch_project_videos` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`project_id` integer NOT NULL,
+	`video_name` text NOT NULL,
+	`video_path` text(1024) NOT NULL,
+	`current_playing` integer DEFAULT false NOT NULL,
+	`subtitle_path` text(1024) NOT NULL,
+	`current_time` integer DEFAULT 0 NOT NULL,
+	`duration` integer DEFAULT 0 NOT NULL,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `dp_watch_projects` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`project_name` text NOT NULL,
+	`type` integer DEFAULT 0 NOT NULL,
+	`project_key` text NOT NULL,
+	`project_path` text(1024) NOT NULL,
+	`current_video_id` integer DEFAULT 0 NOT NULL,
+	`last_watch_time` integer DEFAULT 0 NOT NULL,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `dp_word_translates` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`word` text NOT NULL,
+	`translate` text,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE `dp_words` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`word` text NOT NULL,
+	`stem` text,
+	`translate` text,
+	`note` text,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `dp_kvs_key_unique` ON `dp_kvs` (`key`);--> statement-breakpoint
+CREATE UNIQUE INDEX `dp_sentence_translates_sentence_unique` ON `dp_sentence_translates` (`sentence`);--> statement-breakpoint
+CREATE UNIQUE INDEX `dp_stems_stem_unique` ON `dp_stems` (`stem`);--> statement-breakpoint
+CREATE UNIQUE INDEX `dp_word_translates_word_unique` ON `dp_word_translates` (`word`);--> statement-breakpoint
+CREATE UNIQUE INDEX `dp_words_word_unique` ON `dp_words` (`word`);
