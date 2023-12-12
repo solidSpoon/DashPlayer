2ccb7a61632123ee9dccbb48e2e190aba822f549
solidSpoon <cedarrival@outlook.com>
2023-12-12 20:11:25 +0800

    控制页小窗口样式优化

--- src/renderer/windows/player/pages/PlayerP.tsx
+++ src/renderer/windows/player/pages/PlayerP.tsx
@@ -14,7 +14,6 @@ import BorderProgressBar from '../../../components/BorderProgressBar';
 import useFile from '../../../hooks/useFile';
 import GlobalShortCut from '../../../components/GlobalShortCut';
 import SideBar from '../../../components/SideBar';
-import useSystem from '../../../hooks/useSystem';
 
 const api = window.electron;
 const PlayerP = () => {
@@ -24,8 +23,14 @@ const PlayerP = () => {
         localStorage.getItem('split-size-a') ?? JSON.stringify([75, 25]);
     const sizeB =
         localStorage.getItem('split-size-b') ?? JSON.stringify([80, 20]);
-    const w = cpW.bind(null, useLayout((s) => s.width));
-    const h = cpW.bind(null, useLayout((s) => s.height));
+    const w = cpW.bind(
+        null,
+        useLayout((s) => s.width)
+    );
+    const h = cpW.bind(
+        null,
+        useLayout((s) => s.height)
+    );
     const { videoId } = useParams();
     const playFile = useFile((s) => s.playFile);
     const location = useLocation();
@@ -59,10 +64,12 @@ const PlayerP = () => {
             const rect = posRef.current.getBoundingClientRect();
             setPos({
                 x: rect.x,
-                y: rect.y - titleBarHeight + (window.innerHeight - titleBarHeight) * 0.05,
-                scale: rect.width / window.innerWidth
+                y:
+                    rect.y -
+                    titleBarHeight +
+                    (window.innerHeight - titleBarHeight) * 0.05,
+                scale: rect.width / window.innerWidth,
             });
-            console.log(pos.scale);
         };
         updatePos();
         window.addEventListener('resize', updatePos);
@@ -70,17 +77,28 @@ const PlayerP = () => {
             window.removeEventListener('resize', updatePos);
         };
     }, [titleBarHeight]);
+
+    const showPlayer = w('md') && h('md');
+    const gridTemplate = () => {
+        if (showPlayer && w('lg')) {
+            return '15% 60% 25%';
+        }
+        if (showPlayer) {
+            return '65px calc((100% - 65px) * 12 / 17) calc((100% - 65px) * 5 / 17)';
+        }
+        if (!showPlayer && w('lg')) {
+            return '15% 42.5% 42.5%';
+        }
+        return '65px calc((100% - 65px) * 1 / 2) calc((100% - 65px) * 1 / 2)';
+    };
     return (
         <div
-            className='w-full h-full grid grid-cols-3 grid-rows-2 overflow-hidden bg-gray-100'
+            className="w-full h-full grid grid-cols-3 grid-rows-2 overflow-hidden bg-gray-100"
             style={{
-                gridTemplateColumns: w('lg') ? '15% 60% 25%'
-                    : (h('md') ? '65px calc((100% - 65px) * 12 / 17) calc((100% - 65px) * 5 / 17)'
-                        : '65px calc((100% - 65px) * 1 / 2) calc((100% - 65px) * 1 / 2)'), // 这里定义每列的大小
-                gridTemplateRows: '30% 70%' // 这里定义每行的大小
+                gridTemplateColumns: gridTemplate(),
+                gridTemplateRows: '30% 70%', // 这里定义每行的大小
             }}
         >
-
             <AnimatePresence>
                 {showSideBar && (
                     <>
@@ -90,12 +108,12 @@ const PlayerP = () => {
                             )}
                             initial={{ x: -1000 }}
                             animate={{
-                                x: 0
+                                x: 0,
                             }}
                             exit={{ x: -1000 }}
                             transition={{
                                 type: 'tween',
-                                duration: sideBarAnimation ? 0.2 : 0
+                                duration: sideBarAnimation ? 0.2 : 0,
                             }}
                         >
                             <SideBar compact={!w('lg')} />
@@ -103,16 +121,17 @@ const PlayerP = () => {
                         <motion.div
                             className={cn(
                                 'col-start-2 row-start-1 col-end-4 row-end-3 p-4',
-                                w('md') && 'col-start-3 pl-2',
+                                h('md') && 'row-start-2',
+                                w('md') && 'row-start-1 col-start-3 pl-2'
                             )}
                             initial={{ x: 1000 }}
                             animate={{
-                                x: 0
+                                x: 0,
                             }}
                             exit={{ x: 1000 }}
                             transition={{
                                 type: 'tween',
-                                duration: 0.2
+                                duration: 0.2,
                             }}
                         >
                             <FileBrowser />
@@ -120,20 +139,19 @@ const PlayerP = () => {
 
                         <motion.div
                             className={cn(
-                                'hidden row-start-1 col-start-2 row-end-3 col-end-2 p-4',
-                                w('md') && 'block col-end-2 pr-2',
-                                h('md') && 'block row-end-2 pb-2',
-                                !w('md') && h('md') && 'hidden',
+                                'hidden row-start-1 row-end-3 col-start-2  col-end-4 p-4',
+                                w('md') && 'block col-end-3',
+                                h('md') && 'block row-end-2'
                             )}
                             initial={{ y: -1000 }}
                             animate={{
                                 y: 0,
-                                x: 0
+                                x: 0,
                             }}
                             exit={{ y: -1000 }}
                             transition={{
                                 type: 'tween',
-                                duration: 0.2
+                                duration: 0.2,
                             }}
                         >
                             <ControlBox />
@@ -142,80 +160,72 @@ const PlayerP = () => {
                 )}
             </AnimatePresence>
             <div
-                className={''}
+                className=""
                 ref={posRef}
                 style={{
-                    gridArea: '2 / 2 / 2 / 3'
+                    gridArea: '2 / 2 / 2 / 3',
                 }}
             />
-            {(
+            <div
+                className={cn(
+                    'flex flex-col',
+                    showSideBar && 'p-4 pt-2 pr-2',
+                    !((w('md') && h('md')) || !showSideBar) && 'hidden'
+                )}
+                style={{
+                    gridArea: '1 / 1 / -1 / -1',
+                    transform: showSideBar
+                        ? `translate(${pos.x}px, ${pos.y}px) scale(${pos.scale})`
+                        : 'translate(0px, 0px) scale(1)',
+                    transformOrigin: 'top left',
+                }}
+            >
                 <div
                     className={cn(
-                        'flex flex-col',
-                        showSideBar && 'p-4 pt-2 pr-2',
-                        !(w('md') && h('md') || !showSideBar) && 'hidden',
-                    )}
-                    style={{
-                        // gridArea: showSideBar ? '2 / 2 / 2 / 3' : '1 / 1 / -1 / -1',
-                        gridArea: '1 / 1 / -1 / -1',
-                        transform: showSideBar ? `translate(${pos.x}px, ${pos.y}px) scale(${pos.scale})` : 'translate(0px, 0px) scale(1)',
-                        transformOrigin: 'top left'
-                    }}
-                >
-                    <div
-                        className={cn(
-                            'w-full h-full flex flex-col',
+                        'w-full h-full flex flex-col',
 
-                            showSideBar &&
+                        showSideBar &&
                             'overflow-hidden border-2 border-gray-300 rounded-lg'
+                    )}
+                >
+                    <Split
+                        className={cn(
+                            'flex flex-row h-0 flex-1 w-full bg-background'
                         )}
-                        style={{
-                            // zoom: showSideBar ? 0.65 : 1,
+                        sizes={JSON.parse(sizeA)}
+                        onDragEnd={(sizes) => {
+                            localStorage.setItem(
+                                'split-size-a',
+                                JSON.stringify(sizes)
+                            );
                         }}
                     >
                         <Split
-                            className={cn(
-                                'flex flex-row h-0 flex-1 w-full bg-background'
-                            )}
-                            sizes={JSON.parse(sizeA)}
+                            minSize={10}
+                            className="split z-40"
+                            sizes={JSON.parse(sizeB)}
                             onDragEnd={(sizes) => {
                                 localStorage.setItem(
-                                    'split-size-a',
+                                    'split-size-b',
                                     JSON.stringify(sizes)
                                 );
                             }}
+                            direction="vertical"
                         >
-                            <Split
-                                minSize={10}
-                                className='split z-40'
-                                sizes={JSON.parse(sizeB)}
-                                onDragEnd={(sizes) => {
-                                    localStorage.setItem(
-                                        'split-size-b',
-                                        JSON.stringify(sizes)
-                                    );
-                                }}
-                                direction='vertical'
-                            >
-                                <div className='h-full'>
-                                    <Player />
-                                </div>
-                                <div
-                                    style={{
-                                        // zoom: showSideBar ? 0.65 : 1,
-                                    }}
-                                    className='h-full'>
-                                    <MainSubtitle />
-                                </div>
-                            </Split>
-                            <Subtitle />
+                            <div className="h-full">
+                                <Player />
+                            </div>
+                            <div className="h-full">
+                                <MainSubtitle />
+                            </div>
                         </Split>
-                        <div className={cn('h-2')}>
-                            <BorderProgressBar />
-                        </div>
+                        <Subtitle />
+                    </Split>
+                    <div className={cn('h-2')}>
+                        <BorderProgressBar />
                     </div>
                 </div>
-            )}
+            </div>
             <UploadButton />
             <GlobalShortCut />
         </div>
