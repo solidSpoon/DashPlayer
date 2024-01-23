8048ad9b9e0524db76dc67cf75927f6cd2f28f77
solidSpoon <cedarrival@outlook.com>
2024-01-23 12:55:11 +0800

    migrate 失败时删除数据库重试

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,6 +2,7 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import path from 'path';
 import { app } from 'electron';
+import { and, sql } from 'drizzle-orm';
 
 const file = path.join(
     app?.getPath?.('userData') ?? __dirname,
@@ -11,5 +12,35 @@ const file = path.join(
 const isDev = process.env.NODE_ENV === 'development';
 const sqlite = new Database(file);
 const db = drizzle(sqlite, { logger: isDev });
+
+// 清空数据库
+export async function clearDB() {
+    // Get all tables
+    let tables = await db.select({
+        name: sql<string>`name`,
+    })
+        .from(sql`sqlite_master`)
+        .where(and(sql`type = 'table'`, sql`name != 'sqlite_sequence'`));
+
+    // Drop all tables
+    for (const table of tables.map(e => e.name)) {
+        sqlite.exec(`DROP TABLE ${table}`);
+    }
+
+    // Get all indexes
+    let indexes = await db.select({
+        name: sql<string>`name`,
+    })
+        .from(sql`sqlite_master`)
+        .where(sql`type = 'index'`);
+
+    // Drop all indexes
+    for (const index of indexes.map(e => e.name)) {
+        sqlite.exec(`DROP INDEX ${index}`);
+    }
+
+    // Clear all sequences
+    sqlite.exec(`DELETE FROM sqlite_sequence WHERE 1=1`);
+}
 
 export default db;
--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,7 +1,7 @@
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 import { app } from 'electron';
 import fs from 'fs';
-import db from './db';
+import db, { clearDB } from './db';
 
 const isDev = process.env.NODE_ENV === 'development';
 const config = {
@@ -10,8 +10,15 @@ const config = {
         : `${process.resourcesPath}/drizzle/migrations`,
 };
 const runMigrate = async () => {
-    migrate(db, config);
-};
+    // migrate(db, config);
+    try {
+        migrate(db, config);
+    } catch (error) {
+        console.log('run migrate failed, clear db and retry');
+        await clearDB();
+        migrate(db, config);
+    }
+}
 
 console.log('runMigrate', config);
 console.log('runMigrate', process.resourcesPath);
