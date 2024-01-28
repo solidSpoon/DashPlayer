c28a5c52fa95ac94c059d9cc7eabd6d5d73a826c
solidSpoon <cedarrival@outlook.com>
2024-01-28 13:36:32 +0800

    bugfix: 当数据库文件夹不存在时报错

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,6 +3,7 @@ import Database from 'better-sqlite3';
 import path from 'path';
 import { app } from 'electron';
 import { and, sql } from 'drizzle-orm';
+import fs from 'fs';
 
 const file = path.join(
     app?.getPath?.('userData') ?? __dirname,
@@ -10,32 +11,39 @@ const file = path.join(
     'dp_db.sqlite3'
 );
 const isDev = process.env.NODE_ENV === 'development';
+const dir = path.dirname(file);
+
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+}
 const sqlite = new Database(file);
 const db = drizzle(sqlite, { logger: isDev });
 
 // 清空数据库
 export async function clearDB() {
     // Get all tables
-    let tables = await db.select({
-        name: sql<string>`name`,
-    })
+    const tables = await db
+        .select({
+            name: sql<string>`name`,
+        })
         .from(sql`sqlite_master`)
         .where(and(sql`type = 'table'`, sql`name != 'sqlite_sequence'`));
 
     // Drop all tables
-    for (const table of tables.map(e => e.name)) {
+    for (const table of tables.map((e) => e.name)) {
         sqlite.exec(`DROP TABLE ${table}`);
     }
 
     // Get all indexes
-    let indexes = await db.select({
-        name: sql<string>`name`,
-    })
+    const indexes = await db
+        .select({
+            name: sql<string>`name`,
+        })
         .from(sql`sqlite_master`)
         .where(sql`type = 'index'`);
 
     // Drop all indexes
-    for (const index of indexes.map(e => e.name)) {
+    for (const index of indexes.map((e) => e.name)) {
         sqlite.exec(`DROP INDEX ${index}`);
     }
 
