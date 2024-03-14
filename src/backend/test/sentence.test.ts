cd559cff1c7f650f012fe466164e150ebd7d71bd
solidSpoon <cedarrival@outlook.com>
2024-03-14 19:21:32 +0800

    test

--- src/backend/test/sentence.test.ts
+++ src/backend/test/sentence.test.ts
@@ -1,7 +1,15 @@
 // sum.test.js
 import { expect, test } from 'vitest'
 import { processSentence } from '@/backend/controllers/SubtitleProcesser';
-test("i'm a boy", () => {
-    let sentenceStruct = processSentence("I think that's a good idea.");
+// test("i'm a boy", () => {
+//     let sentenceStruct = processSentence("I think that's a good idea.");
+//     console.log(JSON.stringify(sentenceStruct, null, 2));
+// })
+// test("i'm a boy2", () => {
+//     const sentenceStruct = processSentence("Orson don't cramp the boy's style");
+//     console.log(JSON.stringify(sentenceStruct, null, 2));
+// })
+test("i'm a boy2", () => {
+    const sentenceStruct = processSentence("Have you enjoyed working here at mrs.van de kamp's");
     console.log(JSON.stringify(sentenceStruct, null, 2));
 })
