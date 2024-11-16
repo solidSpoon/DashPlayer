0e55b53d023ca110d107bbcbfc64bba8ee07d6d2
solidSpoon <cedarrival@outlook.com>
2024-11-16 12:20:35 +0800

    整理数据库

--- drizzle/migrations/0000_funny_mauler.sql
+++ drizzle/migrations/0000_funny_mauler.sql
@@ -76,39 +76,13 @@ CREATE TABLE `dp_watch_history` (
 	`project_name` text NOT NULL,
 	`project_path` text NOT NULL,
 	`project_type` integer NOT NULL,
-	`last_watch_time` integer DEFAULT 0 NOT NULL,
+	`current_position` integer DEFAULT 0 NOT NULL,
 	`srt_file` text,
 	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 --> statement-breakpoint
 CREATE INDEX `base_path_file_name_idx` ON `dp_watch_history` (`project_name`,`project_path`);--> statement-breakpoint
-CREATE TABLE `dp_watch_project_videos` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`project_id` integer NOT NULL,
-	`video_name` text NOT NULL,
-	`video_path` text(1024) NOT NULL,
-	`current_playing` integer DEFAULT false NOT NULL,
-	`subtitle_path` text(1024),
-	`current_time` integer DEFAULT 0 NOT NULL,
-	`duration` integer DEFAULT 0 NOT NULL,
-	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `dp_watch_project_videos_project_id_video_path_unique` ON `dp_watch_project_videos` (`project_id`,`video_path`);--> statement-breakpoint
-CREATE TABLE `dp_watch_projects` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`project_name` text NOT NULL,
-	`type` integer DEFAULT 0 NOT NULL,
-	`project_path` text(1024) NOT NULL,
-	`current_playing` integer DEFAULT false NOT NULL,
-	`last_watch_time` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `dp_watch_projects_project_path_unique` ON `dp_watch_projects` (`project_path`);--> statement-breakpoint
 CREATE TABLE `dp_word_translates` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`word` text NOT NULL,
