72d396799e959619e54e5f5cd3e31de151031115
solidSpoon <cedarrival@outlook.com>
2024-12-28 16:30:05 +0800

    调整 mp3 显示

--- src/fronted/components/fileBowser/project-list-card.tsx
+++ src/fronted/components/fileBowser/project-list-card.tsx
@@ -1,8 +1,8 @@
 import useSWR from 'swr';
 import { cn } from '@/fronted/lib/utils';
 import React from 'react';
-import { SWR_KEY, swrApiMutate, swrMutate } from '@/fronted/lib/swr-util';
-import { Film, ListVideo, Trash2 } from 'lucide-react';
+import { SWR_KEY, swrApiMutate } from '@/fronted/lib/swr-util';
+import { Film, ListVideo, Trash2, Music } from 'lucide-react'; // 注意这里导入了 Music 图标
 import { Button } from '@/fronted/components/ui/button';
 import TimeUtil from '@/common/utils/TimeUtil';
 import { Progress } from '@/fronted/components/ui/progress';
@@ -15,22 +15,41 @@ import {
 import UrlUtil from '@/common/utils/UrlUtil';
 import WatchHistoryVO from '@/common/types/WatchHistoryVO';
 import PathUtil from '@/common/utils/PathUtil';
+import FileUtil from '@/backend/utils/FileUtil';
+import MediaUtil from '@/common/utils/MediaUtil';
+import MusicCard from '@/fronted/components/fileBowser/music-card';
 
 const api = window.electron;
 
-const ProjectListCard = ({ video, onSelected }: {
-    video: WatchHistoryVO,
-    className?: string
+const ProjectListCard = ({
+                             video,
+                             onSelected
+                         }: {
+    video: WatchHistoryVO;
+    className?: string;
     onSelected: () => void;
 }) => {
-    const { data: url } = useSWR([SWR_KEY.SPLIT_VIDEO_THUMBNAIL, video.basePath, video.fileName, video.current_position] ,
+    // 1. 检测是否是 mp3
+    const isAudio = MediaUtil.isAudio(video.fileName);
+
+    // 2. 如果是 mp3，就不调用生成缩略图的接口，把 key 设为 null
+    const { data: url } = useSWR(
+        !isAudio
+            ? [SWR_KEY.SPLIT_VIDEO_THUMBNAIL, video.basePath, video.fileName, video.current_position]
+            : null,
         async ([_key, path, file, time]) => {
-            return await api.call('split-video/thumbnail', { filePath: PathUtil.join(path, file), time });
+            return await api.call('split-video/thumbnail', {
+                filePath: PathUtil.join(path, file),
+                time
+            });
         }
     );
+
     console.log('video', video);
+
     const [hover, setHover] = React.useState(false);
     const [contextMenu, setContextMenu] = React.useState(false);
+
     return (
         <ContextMenu
             onOpenChange={(open) => {
@@ -44,37 +63,62 @@ const ProjectListCard = ({ video, onSelected }: {
                     className={cn('')}
                     onClick={onSelected}
                 >
-                    <div className={cn('relative w-full rounded-lg overflow-hidden border-none')}>
-                        {url ? <img
-                            src={UrlUtil.file(url)}
-                            style={{
-                                aspectRatio: '16/9'
-                            }}
-                            className={cn('w-full object-cover', (hover || contextMenu) && 'filter brightness-75')}
-                            alt={video.fileName}
-                        /> : <div
-                            style={{
-                                aspectRatio: '16/9'
-                            }}
-                            className={'w-full bg-gray-500 flex items-center justify-center'}>
-                            <Film className={'w-8 h-8'} />
-                        </div>}
+                    <div
+                        className={cn('relative w-full rounded-lg overflow-hidden border-none')}
+                    >
+                        {/* 3. 判断如果是 mp3，优先展示 Music 图标，否则还是原先逻辑 */}
+                        {isAudio ? (
+                            <MusicCard fileName={video.fileName}/>
+                        ) : url ? (
+                            <img
+                                src={UrlUtil.file(url)}
+                                style={{
+                                    aspectRatio: '16/9'
+                                }}
+                                className={cn(
+                                    'w-full object-cover',
+                                    (hover || contextMenu) && 'filter brightness-75'
+                                )}
+                                alt={video.fileName}
+                            />
+                        ) : (
+                            <div
+                                style={{
+                                    aspectRatio: '16/9'
+                                }}
+                                className="w-full bg-gray-500 flex items-center justify-center"
+                            >
+                                <Film className="w-8 h-8" />
+                            </div>
+                        )}
+
                         <div
-                            className={cn('absolute bottom-2 right-2 text-white bg-black bg-opacity-80 rounded-md p-1 py-0.5 text-xs flex')}>
-                            {!video.isFolder ? TimeUtil.secondToTimeStrCompact(video?.duration) : <>
-                                <ListVideo className={'w-4 h-4'} /></>}
+                            className={cn(
+                                'absolute bottom-2 right-2 text-white bg-black bg-opacity-80 rounded-md p-1 py-0.5 text-xs flex'
+                            )}
+                        >
+                            {/* 如果是文件夹，就用 ListVideo 图标；如果不是文件夹，就展示时长 */}
+                            {!video.isFolder ? (
+                                TimeUtil.secondToTimeStrCompact(video?.duration)
+                            ) : (
+                                <ListVideo className="w-4 h-4" />
+                            )}
                         </div>
+
+                        {/* 进度条 */}
                         <Progress
                             className={cn('absolute bottom-0 left-0 w-full rounded-none h-1 bg-gray-500')}
-                            value={Math.floor((video?.current_position || 0) / (video?.duration || 1) * 100)}
+                            value={Math.floor(
+                                ((video?.current_position || 0) / (video?.duration || 1)) * 100
+                            )}
                         />
 
-
+                        {/* 悬浮时展示删除按钮 */}
                         {hover && (
                             <Button
-                                className={'absolute top-2 right-2 w-6 h-6 bg-background'}
-                                size={'icon'}
-                                variant={'ghost'}
+                                className="absolute top-2 right-2 w-6 h-6 bg-background"
+                                size="icon"
+                                variant="ghost"
                                 onClick={async (e) => {
                                     e.stopPropagation();
                                     console.log('swrdelete', video.id);
@@ -82,16 +126,19 @@ const ProjectListCard = ({ video, onSelected }: {
                                     await swrApiMutate('watch-history/list');
                                 }}
                             >
-                                <Trash2
-                                    className={'w-3 h-3'}
-                                />
+                                <Trash2 className="w-3 h-3" />
                             </Button>
                         )}
                     </div>
 
                     <div
-                        className={cn('w-full line-clamp-2 break-words', (hover || contextMenu) && 'underline')}
-                    >{video.fileName}</div>
+                        className={cn(
+                            'w-full line-clamp-2 break-words',
+                            (hover || contextMenu) && 'underline'
+                        )}
+                    >
+                        {video.fileName}
+                    </div>
                 </div>
             </ContextMenuTrigger>
             <ContextMenuContent>
@@ -99,16 +146,19 @@ const ProjectListCard = ({ video, onSelected }: {
                     onClick={async () => {
                         await api.call('system/open-folder', video.basePath);
                     }}
-                >Show In Explorer</ContextMenuItem>
+                >
+                    Show In Explorer
+                </ContextMenuItem>
                 <ContextMenuItem
                     onClick={async () => {
                         await api.call('watch-history/group-delete', video.id);
                         await swrApiMutate('watch-history/list');
                     }}
-                >Delete</ContextMenuItem>
+                >
+                    Delete
+                </ContextMenuItem>
             </ContextMenuContent>
         </ContextMenu>
-
     );
 };
 
