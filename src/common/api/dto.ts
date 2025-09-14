763eaa16959de0688ef5c318940c53dd000b8ce9
solidSpoon <cedarrival@outlook.com>
2025-09-14 09:00:31 +0800

    单词管理backup

--- src/common/api/dto.ts
+++ src/common/api/dto.ts
@@ -3,5 +3,9 @@ import { DateRange } from "react-day-picker";
 export type ClipQuery = {
     keyword: string;
     keywordRange: 'context' | 'clip';
+    tags: number[];
+    tagsRelation: 'and' | 'or';
     date: DateRange;
+    includeNoTag: boolean;
 }
+
