fc42975007c4ba651df4b9f8b6840ba311f456cd
solidSpoon <cedarrival@outlook.com>
2025-12-21 12:52:16 +0800

    react complier

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -2,9 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
+const enableReactCompiler = process.env.REACT_COMPILER === '1';
+
 // https://vitejs.dev/config
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(
+            enableReactCompiler
+                ? {
+                    babel: {
+                        plugins: ['babel-plugin-react-compiler'],
+                    },
+                }
+                : undefined
+        ),
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
